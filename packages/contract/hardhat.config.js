e0260bc5d4d677119ef1382284dccb7db20438ae
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-21 20:16:35 +0900

    feat: S2-L2まで

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
