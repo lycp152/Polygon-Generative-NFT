e793af340fbae04ed5212d22ad03fe09bfe26017
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-21 20:39:27 +0900

    feat: S2-L4まで

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API } = process.env;
 
 module.exports = {
   solidity: "0.8.18",
@@ -11,5 +12,8 @@ module.exports = {
       url: API_URL,
       accounts: [PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API,
   },
 };
